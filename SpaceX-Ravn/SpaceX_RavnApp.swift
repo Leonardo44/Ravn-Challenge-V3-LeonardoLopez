53c8f9807f5c66291c0b6128693a371c485d6984
leonarrdo404 <leo@ese.plus>
2023-11-30 19:51:54 -0600

    Initial Commit

---
+++ SpaceX-Ravn/SpaceX_RavnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_RavnApp.swift
+//  SpaceX-Ravn
+//
+//  Created by Leo on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_RavnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
